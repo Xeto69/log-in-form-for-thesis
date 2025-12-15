ff5e2a0e338b92b38efa50c19290e568f91daed6
Xeto69 <jlibanez666@gmail.com>
2025-12-15 23:54:20 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,43 +1,24 @@
 const form = document.getElementById("loginForm");
 const errorMsg = document.getElementById("errorMsg");
 
-function togglePassword() {
-  const pass = document.getElementById("password");
-  pass.type = pass.type === "password" ? "text" : "password";
+if (form) {
+  form.addEventListener("submit", function (e) {
+    e.preventDefault();
+
+    const user = document.getElementById("username").value;
+    const pass = document.getElementById("password").value;
+
+    if (user === "admin" && pass === "1234") {
+      localStorage.setItem("loggedIn", "true");
+      window.location.href = "dashboard.html";
+    } else {
+      errorMsg.textContent = "Invalid username or password";
+    }
+  });
 }
 
-form.addEventListener("submit", function (e) {
-  e.preventDefault();
-
-  const user = document.getElementById("username").value;
-  const pass = document.getElementById("password").value;
-
-  if (user === "admin" && pass === "1234") {
-    alert("Login Successful!");
-    errorMsg.textContent = "";
-  } else {
-    errorMsg.textContent = "Invalid username or password";
-  }
-});
-
-function openForgot() {
-  document.getElementById("forgotModal").style.display = "flex";
+// LOGOUT
+function logout() {
+  localStorage.removeItem("loggedIn");
+  window.location.href = "index.html";
 }
-
-function closeForgot() {
-  document.getElementById("forgotModal").style.display = "none";
-}
-
-function openSignup() {
-  document.getElementById("signupModal").style.display = "flex";
-}
-
-function closeSignup() {
-  document.getElementById("signupModal").style.display = "none";
-}
-
-window.onclick = function(e) {
-  if (e.target.classList.contains("modal")) {
-    e.target.style.display = "none";
-  }
-};
